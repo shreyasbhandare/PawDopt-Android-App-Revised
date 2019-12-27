9aeee9e1f5fb3e3a2abdc520206b1e7a87e8b0e7
Shreyas Bhandare <ssb146@scarletmail.rutgers.edu>
2019-12-27 18:42:25 -0500

    sign up and login activity layouts

--- app/src/main/java/com/sbhandare/pawdopt/LoginActivity.java
+++ app/src/main/java/com/sbhandare/pawdopt/LoginActivity.java
@@ -1,14 +1,29 @@
 package com.sbhandare.pawdopt;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView signUpTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUpTxt = findViewById(R.id.signUpTxt);
+
+        signUpTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+                LoginActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/sbhandare/pawdopt/SignUpActivity.java
+++ app/src/main/java/com/sbhandare/pawdopt/SignUpActivity.java
@@ -2,13 +2,29 @@ package com.sbhandare.pawdopt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private TextView loginTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        loginTxt = findViewById(R.id.loginTxt);
+
+        loginTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(SignUpActivity.this, LoginActivity.class);
+                SignUpActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
